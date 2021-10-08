50add6a67edbc2a5b92dec0abccfcede40b522a8
ada2030 <osorovadahan04@gmail.com>
2021-10-08 10:28:20 +0300

    4.9. Больше деталей (правки).

--- js/main.js
+++ js/main.js
@@ -1,110 +1,91 @@
-const getRandomNumber = (from, to) => {
-  if (from <=0 || to <= 0) {
-    throw new Error('Диапазон должен быть выше нуля');
-  }
-  if (from >= to) {
-    throw new Error('Минимальное число должно быть меньше чем максимальное');
-  }
-  return Math.floor(Math.random() * (to - from + 1) + from);
-};
+const SIMILAR_COUNT = 10;
 
-getRandomNumber(4, 5);
-
-const getRandomFloatingPointNumber = (from, to, floatNumber) => {
-  if (from < 0 || to < 0) {
-    throw new Error('Диапазон должен быть выше нуля');
-  }
-  if (from >= to) {
-    throw new Error('Минимальное число должно быть меньше чем максимальное');
-  }
-  return Math.random() * (to - from + 1) + from.toFixed(floatNumber);
-};
-
-getRandomFloatingPointNumber(4, 5, 2);
-
-const TITLE = [
-  'Предлагается стильная двухкомнатная квартира',
-  'Квартира с просторной планировкой',
-  'Уникальные апартаменты с панорамными видами',
-  'Без комиссии для арендатора.',
-  'Функциональные апартаменты с панорамным остеклением',
-  'Квартира с современным дизайнерским ремонтом',
-  'Апартаменты в Москва Сити с потрясающими видами',
-  'Функциональные апартаменты с дизайнерской отделкой, уютным интерьером',
-  'Искали самый козырный апартамент в Неве?',
-  'Угловой Апартамент в Москва Сити',
-];
-
-const TYPE = [
-  'palace',
-  'flat',
-  'house',
-  'bungalow.',
-  'hotel',
-];
-
-const CHECKIN_CHECKOUT = [
-  '12:00',
-  '13:00',
-  '14:00',
-];
-
-const FEATURES = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-
-const getFeatures = () => {
-  const RANDOM_FEATURES = [];
-  for (let i = 0; i < Math.floor(Math.random() * (FEATURES.length - 1 + 1) + 1); i++) {
-    RANDOM_FEATURES[i] = FEATURES[i];
-  }
-  return RANDOM_FEATURES;
-};
-
-const DESCRIPTION = [
-  'Встроенная кухня - 10 кв.м. (посудомоечная машина, микроволновая, вытяжка, чайник, набор посуды)',
-  'Квартира полностью меблированная, в каждой комнате встроенный шкаф',
-  'Дом новый, хороший подъезд, домофон, 2 лифта',
-  'В квартире выполнен качественный ремонт, на стенах светлые обои, пол ламинат, высокие потолки',
-];
-
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
-
-const getPhotos = () => {
-  const RANDOM_PHOTOS = [];
-  for (let i = 0; i < Math.floor(Math.random() * (10 - 1 + 1) + 1); i++) {
-    RANDOM_PHOTOS[i] = PHOTOS[Math.floor(Math.random() * (2 - 0 + 1) + 0)];
-  }
-  return RANDOM_PHOTOS;
-};
-
-const SIMILAR_WIZARD_COUNT = 1;
-
-const getRandomArrayElement = (elements) => {
-  return elements[Math.floor(Math.random() * (0 - elements.length + 1)) + elements.length];
-};
-
-const author = () => {
+const getAuthor = () => {
   const IMG_NAME = 'img/avatars/user';
   const FORMAT = '.png';
+  const ZERO = '0';
+  const NUMBER = Math.floor(Math.random() * (10 - 1 + 1) + 1);
+  if (NUMBER === 10) {
+    return {
+      avatar: IMG_NAME + NUMBER + FORMAT,
+    };
+  }
   return {
-    avatar: IMG_NAME + Math.floor(Math.random() * (10 - 1 + 1) + 1) + FORMAT,
+    avatar: IMG_NAME + ZERO + NUMBER + FORMAT,
   };
 };
 
-const offer = () => {
+const getLocation = () => {
+  return {
+    lat: (Math.random() * (35.70000 - 35.65000 + 1) + 35.65000).toFixed(5),
+    lng: (Math.random() * (139.80000 - 139.70000 + 1) + 139.70000).toFixed(5),
+  };
+};
+
+const getOffer = () => {
+  const TITLE = [
+    'Предлагается стильная двухкомнатная квартира',
+    'Квартира с просторной планировкой',
+    'Уникальные апартаменты с панорамными видами',
+    'Без комиссии для арендатора.',
+    'Функциональные апартаменты с панорамным остеклением',
+    'Квартира с современным дизайнерским ремонтом',
+    'Апартаменты в Москва Сити с потрясающими видами',
+    'Функциональные апартаменты с дизайнерской отделкой, уютным интерьером',
+    'Искали самый козырный апартамент в Неве?',
+    'Угловой Апартамент в Москва Сити',
+  ];
+  const TYPE = [
+    'palace',
+    'flat',
+    'house',
+    'bungalow.',
+    'hotel',
+  ];
+  const CHECKIN_CHECKOUT = [
+    '12:00',
+    '13:00',
+    '14:00',
+  ];
+  const DESCRIPTION = [
+    'Встроенная кухня - 10 кв.м. (посудомоечная машина, микроволновая, вытяжка, чайник, набор посуды)',
+    'Квартира полностью меблированная, в каждой комнате встроенный шкаф',
+    'Дом новый, хороший подъезд, домофон, 2 лифта',
+    'В квартире выполнен качественный ремонт, на стенах светлые обои, пол ламинат, высокие потолки',
+  ];
+  const getFeatures = () => {
+    const FEATURES = [
+      'wifi',
+      'dishwasher',
+      'parking',
+      'washer',
+      'elevator',
+      'conditioner',
+    ];
+    const RANDOM_FEATURES = [];
+    for (let i = 0; i < Math.floor(Math.random() * (FEATURES.length - 1 + 1) + 1); i++) {
+      RANDOM_FEATURES[i] = FEATURES[i];
+    }
+    return RANDOM_FEATURES;
+  };
+  const getPhotos = () => {
+    const PHOTOS = [
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+      'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+    ];
+    const RANDOM_PHOTOS = [];
+    for (let i = 0; i < Math.floor(Math.random() * (10 - 1 + 1) + 1); i++) {
+      RANDOM_PHOTOS[i] = PHOTOS[Math.floor(Math.random() * (2 - 0 + 1) + 0)];
+    }
+    return RANDOM_PHOTOS;
+  };
+  const getRandomArrayElement = (elements) => {
+    return elements[Math.floor(Math.random() * (0 - elements.length + 1)) + elements.length];
+  };
   return {
     title: getRandomArrayElement(TITLE),
-    address: String(Math.floor(Math.random() * (180 - (-180) + 1) + (-180))) + ',' + String(Math.floor(Math.random() * (90 - (-90) + 1) + (-90))),
+    address: getLocation(),
     price: Math.floor(Math.random() * (100000 - 1 + 1) + 1),
     type: getRandomArrayElement(TYPE),
     rooms: Math.floor(Math.random() * (10 - 1 + 1) + 1),
@@ -117,16 +98,9 @@ const offer = () => {
   };
 };
 
-const location = () => {
-  return {
-    lat: (Math.random() * (35.70000 - 35.65000 + 1) + 35.65000).toFixed(5),
-    lng: (Math.random() * (139.80000 - 139.70000 + 1) + 139.70000).toFixed(5),
-  };
-};
-
-let randomArray = [];
 const mainArray = () => {
-  return randomArray = Array.from({length: 1}, author).concat(Array.from({length: 1}, offer).concat(Array.from({length: 1}, location)));
+  return Array.from({length: 1}, getAuthor).concat(Array.from({length: 1}, getOffer).concat(Array.from({length: 1}, getLocation)));
 };
 
-const SIMILAR_ANNOUNCEMENT = Array.from({length: SIMILAR_WIZARD_COUNT}, mainArray);
+const SIMILAR_ANNOUNCEMENT = Array.from({length: SIMILAR_COUNT}, mainArray);
+SIMILAR_ANNOUNCEMENT();
